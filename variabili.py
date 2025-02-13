c862302e5dfddeba0c4e2d3ef24893637ab46ca7
antonionastri <nastritoni98@gmail.com>
2025-02-13 19:19:51 +0100

    python_corso

---
+++ variabili.py
@@ -0,0 +1,42 @@
+# variabili
+nome_completo = "gianni rossi"
+colore = "rosso"
+
+print(colore)
+
+colore = "giallo"
+
+print(colore)
+
+numero = 9
+
+#tipi dato
+stringa = "ciao" # stringa - str
+intero = 9 # int
+attivo = True # bool
+decimale = 9.9 # float
+
+print(type(intero))
+print(type(attivo))
+
+totale = 10 + numero
+print(totale)
+
+numero2 = "9"
+#totale = 10 + numero2 -errore
+
+#conversione tipo di dato (casting)
+n = int(numero2)
+totale = 10 + n
+
+print(totale)
+
+s = numero2 + colore
+print(s)
+
+t = "900" + "30"
+print(t)
+
+# sequenze
+
+lista = [1,2,3] # list
